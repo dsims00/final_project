5a94eec0755a2bc043f55fb0d452df500f0e17a6
Dan Simakovsky <daniel.simakovsky@gmail.com>
2016-01-31 14:51:16 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// window.addEventListener('load', function){
+// 	document.getElementById("container").style.opacity = "0.5";
+// }
+
+
+	
+
+
+
+
+
+
+
+// setTimeout (function() {
+//  document.getElementById(‘popup’)
+// }, 2000)
+
+// function changeOpacity() {
+//     document.getElementById("popup").style.opacity = "0.4";
+
+// }
+
+
+
+
+// setTimeout(function() {
+// 	document.getElementById('popup').style.opacity = "0.0";
+// }, 3000);	
+
+// window.onload=changeOpacity;
+
+
+	
