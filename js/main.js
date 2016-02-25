257bdfeea69d32b4510eb6ca0a87e2bf31fa50df
dsims00 <daniel.simakovsky@gmail.com>
2016-02-25 16:18:29 -0500

    Updating gh-pages from master branch via GitBot

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// window.addEventListener('load', function){
+// 	document.getElementById("container").style.opacity = "0.5";
+// }
+
+
+	
+
+
+
+
+
+
+
+// setTimeout (function() {
+//  document.getElementById(‘popup’)
+// }, 2000)
+
+// function changeOpacity() {
+//     document.getElementById("popup").style.opacity = "0.4";
+
+// }
+
+function hidePopup() {
+	setTimeout(function() {
+	document.getElementById('popup').style.opacity = "0.0";
+}, 1000);	
+}
+
+function goToGoogle() {
+	window.location.replace("http://www.google.com");
+}
+
+
+
+// window.onload=changeOpacity;
+
+
+	
